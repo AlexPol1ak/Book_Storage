b217e2292dcae6879541bd2473e50182f08a0d33
Alex <zarjrw1994@gmail.com>
2023-12-07 14:46:04 +0300

    Implemented a class of user restrictions according to the user's status.

--- src/auth/utils/constraints/constraint.py
+++ src/auth/utils/constraints/constraint.py
@@ -1,16 +1,29 @@
-# from dataclasses import dataclass
-#
-#
-# @dataclass(frozen=True)
-# class Constraint:
-#     def __init__(self, **kwargs):
-#         for key, value in kwargs.items():
-#             self.__dict__[key] = value
-#
-#     def __str__(self):
-#         return self.__dict__.__str__()
-#
-#
-# c = Constraint(n=1, b='dsf')
-# print(c)
-# c.b = 4
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Constraint:
+    """Provides data about the user's limitations."""
+    name: str
+    max_size_text_file: int
+    max_count_text_file: int
+    max_count_text_file_in_collection: int
+    max_count_collections_created: int
+    max_count_collections_subscriptions: int
+    max_count_groups_created: int
+    max_count_groups_subscriptions: int
+
+    def __post_init__(self):
+        fields_to_check = [
+            "max_size_text_file",
+            "max_count_text_file",
+            "max_count_text_file_in_collection",
+            "max_count_collections_created",
+            "max_count_collections_subscriptions",
+            "max_count_groups_created",
+            "max_count_groups_subscriptions"
+        ]
+        for field_name in fields_to_check:
+            value = getattr(self, field_name)
+            if value == -1 or value > 0:
+                raise ValueError(f"{field_name} The value can be -1 or greater than 0")
---
+++ src/auth/utils/constraints/limits.py
@@ -0,0 +1,43 @@
+from auth.utils.constraints.constraint import Constraint
+from typing import Type
+
+
+class Limits:
+    def __init__(self):
+        self.constraints: dict[str, Constraint] = dict()
+
+    def add_constraint(self,
+                       name: str,
+                       max_size_text_file: int,
+                       max_count_text_file: int,
+                       max_count_text_file_in_collection: int,
+                       max_count_collections_created: int,
+                       max_count_collections_subscriptions: int,
+                       max_count_groups_created: int,
+                       max_count_groups_subscriptions: int
+                       ):
+        """Sets limits."""
+        constraint_obj = Constraint(name,
+                                    max_size_text_file,
+                                    max_count_text_file,
+                                    max_count_text_file_in_collection,
+                                    max_count_collections_created,
+                                    max_count_collections_subscriptions,
+                                    max_count_groups_created,
+                                    max_count_groups_subscriptions
+
+                                    )
+        self.constraints[name] = constraint_obj
+
+    def __str__(self):
+        st = ""
+        for key, value in self.constraints.items():
+            st += f"{key}: {str(value)}\n"
+
+        return st
+
+    def __call__(self, status: str) -> Constraint:
+        """Returns the constraint according to the status."""
+        if status not in self.constraints:
+            raise ValueError(f"There are no restrictions added to this {status}.")
+        return self.constraints[status]
