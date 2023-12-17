f00646df755dd0a04442d5039f57a1aa40f0a019
Alex <zarjrw1994@gmail.com>
2023-12-17 21:27:56 +0300

    Added a path operation to quickly update a user's status.

--- src/user/routers/privileged_users.py
+++ src/user/routers/privileged_users.py
@@ -1,4 +1,5 @@
-from typing import Annotated
+from enum import Enum
+from typing import Annotated, Type
 
 from fastapi import APIRouter, HTTPException, Path, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -31,11 +32,11 @@ async def user_update(user_id: Annotated[int, Path(qe=1)],
                                                f"Available statuses: {', '.join(user_statuses)}.",
                                     'statuses': user_statuses,
                                 })
-
-    user_db = await crud.update_user(session, data, user_id)
-    if user_db is None:
+    try:
+        user_db = await crud.update_user(session, data, user_id)
+    except ValueError as ex:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail={"message": f"User id: {user_id} not found"})
+                            detail={"message": f"{ex}"})
 
     return UserReadFullScheme.model_validate(user_db)
 
@@ -58,3 +59,30 @@ async def user_delete(user_id: Annotated[int, Path(ge=1)],
         return {"result": flag}
 
 
+@admin_router.get('/user/edit-status/{user_id}/{new_status}')
+async def edit_status_user(user_id: Annotated[int, Path(ge=1)],
+                           new_status: str,
+                           auth_user=Depends(current_user),
+                           session: AsyncSession = Depends(get_async_session)
+                           ):
+    if not auth_user.is_superuser:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail={"message": "Not enough rights to perform the operation."})
+
+    statuses: list = await crud.collection_statuses(session, view='list')
+    if new_status not in statuses:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail={
+                                "message": f"The status of {new_status} is not available. "
+                                           f"Available statuses: {', '.join(statuses)}.",
+                                'statuses': statuses,
+                            })
+
+    update_scheme = UserUpdateFullScheme(status=new_status)
+    try:
+        user_db = await crud.update_user(session, update_scheme, user_id)
+    except ValueError as ex:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail={"message": f"{ex}"})
+
+    return UserReadFullScheme.model_validate(user_db)
