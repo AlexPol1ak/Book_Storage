a2a6c5fd37d5ff857702a7e858d22c0afa51f205
Alex <zarjrw1994@gmail.com>
2024-01-22 19:57:51 +0300

    Refactoring. Changing the definition of the category creation path. Creating categories via category manager object.

--- src/category/routers/privileged_users.py
+++ src/category/routers/privileged_users.py
@@ -1,20 +1,19 @@
 # Routes for privileged users.
 
-
 from typing import Annotated, Type
-
 from fastapi import APIRouter, HTTPException, Path, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
-from category import crud
+from category.manger import CategoryManager
 from category.schema import CategoryCreateScheme, CategoryReadScheme
 from config import STORAGE
 from database import get_async_session
 from user.auth_config import current_user
-from utils.text_formatter import name_formatter
 
 p_category_router = APIRouter(tags=['Admin and superuser', 'Category'], prefix='/category')
+
+category_manager = CategoryManager(STORAGE)
 
 
 @p_category_router.post('/create/', status_code=status.HTTP_201_CREATED)
@@ -26,12 +25,7 @@ async def create_category(
 
     if auth_user.is_superuser or auth_user.is_admin:
         try:
-            name = await name_formatter(data.name, punctuation_del=True)
-            category_system_name = await name_formatter(name, translit_text='ru-en',
-                                                        replace_space="_")
-            category_path = await STORAGE.add_category(category_system_name)
-            category_obj = await crud.create_category(session, name, category_system_name, data.description,
-                                                      category_path, auth_user.id)
+            category_obj = await category_manager.create(session, data.name, data.description, auth_user.id)
             return CategoryReadScheme.model_validate(category_obj)
         except (FileExistsError, ValueError):
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
