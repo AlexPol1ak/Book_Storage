035fc19d1f06464a4d8cfede7d940df30dc45c61
Alex <zarjrw1994@gmail.com>
2024-01-27 18:31:53 +0300

    Modified: Checking the user's authorization to perform actions is not done in the body of the path operation function, but in the dependency .

--- src/category/routers/privileged_users.py
+++ src/category/routers/privileged_users.py
@@ -20,41 +20,34 @@ category_manager = CategoryManager(STORAGE)
 @p_category_router.post('/create/', status_code=status.HTTP_201_CREATED)
 async def create_category(
         data: CategoryCreateScheme,
-        auth_user=Depends(current_user),
+        auth_user=Depends(is_superuser_or_admin),
         session: AsyncSession = Depends(get_async_session)) -> CategoryReadScheme:
     """Creates a new category and returns information about it."""
 
-    if auth_user.is_superuser or auth_user.is_admin:
-        try:
-            category_dict = await category_manager.create(session, data.name, data.description, auth_user.id)
-            return CategoryReadScheme(name=category_dict.get('name'),
-                                      description=category_dict.get('description'),
-                                      data_joined=category_dict.get('date_joined'),
-                                      )
-        except (FileExistsError, ValueError):
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                                detail=f"Category {data.name} exists")
-    else:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                            detail=f"Not enough authority.")
+    try:
+        category_dict = await category_manager.create(session, data.name, data.description, auth_user.id)
+        return CategoryReadScheme(name=category_dict.get('name'),
+                                  description=category_dict.get('description'),
+                                  data_joined=category_dict.get('date_joined'),
+                                  )
+    except (FileExistsError, ValueError):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"Category {data.name} exists")
 
 
 @p_category_router.patch('/update/{category_name_or_id}')
 async def update_category(category_name_or_id: str | int,
                           data: CategoryUpdateScheme,
-                          auth_user=Depends(current_user),
+                          auth_user=Depends(is_superuser_or_admin),
                           session: AsyncSession = Depends(get_async_session)) -> CategoryReadScheme:
     """Updates the category name or description and returns information about the category."""
-    if auth_user.is_superuser or auth_user.is_admin:
-        category_dict = await category_manager.update(session, category_name_or_id,
-                                                      new_name=data.name, new_description=data.description)
-        return CategoryReadScheme(name=category_dict.get('name'),
-                                  description=category_dict.get('description'),
-                                  data_joined=category_dict.get('date_joined'),
-                                  )
-    else:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                            detail=f"Not enough authority.")
+# Обработать ошибки.!!!!!!!!!!!!1
+    category_dict = await category_manager.update(session, category_name_or_id,
+                                                  new_name=data.name, new_description=data.description)
+    return CategoryReadScheme(name=category_dict.get('name'),
+                              description=category_dict.get('description'),
+                              data_joined=category_dict.get('date_joined'),
+                              )
 
 
 @p_category_router.delete('/delete/{category_name_or_id}', status_code=status.HTTP_204_NO_CONTENT)
