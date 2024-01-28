889ce8ceab7738936c588817958a3711986701f2
Alex <zarjrw1994@gmail.com>
2024-01-28 23:39:39 +0300

    In the category update path operation, exceptions are handled.

--- src/category/routers/privileged_users.py
+++ src/category/routers/privileged_users.py
@@ -5,16 +5,13 @@ from fastapi import APIRouter, HTTPException, Path, Depends, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
-from category.manager import CategoryManager
+from category.manager import category_manager
 from category.schema import CategoryCreateScheme, CategoryReadScheme, CategoryUpdateScheme
 from config import STORAGE
 from database import get_async_session
-from user.auth_config import current_user
 from user.user_dependencies import is_superuser_or_admin
 
 p_category_router = APIRouter(tags=['Admin and superuser', 'Category'], prefix='/category')
-
-category_manager = CategoryManager(STORAGE)
 
 
 @p_category_router.post('/create/', status_code=status.HTTP_201_CREATED)
@@ -41,13 +38,20 @@ async def update_category(category_name_or_id: str | int,
                           auth_user=Depends(is_superuser_or_admin),
                           session: AsyncSession = Depends(get_async_session)) -> CategoryReadScheme:
     """Updates the category name or description and returns information about the category."""
-# Обработать ошибки.!!!!!!!!!!!!1
-    category_dict = await category_manager.update(session, category_name_or_id,
-                                                  new_name=data.name, new_description=data.description)
-    return CategoryReadScheme(name=category_dict.get('name'),
-                              description=category_dict.get('description'),
-                              data_joined=category_dict.get('date_joined'),
-                              )
+    try:
+        category_dict = await category_manager.update(session, category_name_or_id,
+                                                      new_name=data.name, new_description=data.description)
+        return CategoryReadScheme(name=category_dict.get('name'),
+                                  description=category_dict.get('description'),
+                                  data_joined=category_dict.get('date_joined'),
+                                  )
+    except NotADirectoryError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Category {category_name_or_id} not found.")
+    except IsADirectoryError:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail=f"Category {category_name_or_id} is not empty.")
+
 
 
 @p_category_router.delete('/delete/{category_name_or_id}', status_code=status.HTTP_204_NO_CONTENT)
